1947bc2607c2b7810fdb51be2da5a11068421f39
xueming li <perfecthm56@gmail.com>
2013-03-17 01:48:47 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImportEmailContacts::Application.config.secret_token = 'eedae651a71985cf5b00233457aa668fbb818e6c598be6ca9d4b7664f26bdea1c3cd75c3b8e2e8246d58ee63572a1388054119842537f35d3fb529bdc18131e9'
